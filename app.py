110f0669a12e91bd1e8d4616d68d6f6003fbcc92
dilshappmstone <dilsha.pmstone@gmail.com>
2026-04-23 16:29:38 +1000

    New

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.enums import TA_CENTER, TA_RIGHT
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 # =========================
 # CONSTANTS
